16e6c76284dacb12e44f2d568fcf0cfe23cd5e89
vikas9872 <shanabhogvikas648.com>
2025-06-08 19:05:12 +0530

    yet to complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import { useEffect } from 'react';
 import { ResumeProvider } from './Context';
 import './App.css';
-import WebFont from 'webfontloader';
 import Main from './Components/Main';
 
 function App() {
-
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ['Pacifico', 'Poppins']
-      }
-    });
-  }, []);
-
   return (
     <>
       <ResumeProvider>
